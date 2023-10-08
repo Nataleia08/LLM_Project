a29cc1718a34aec34777b6cf35d80e6415dfff5d
PolinaDyka0 <115357542+PolinaDyka0@users.noreply.github.com>
2023-10-08 13:55:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ app.include_router(chat.router, prefix="/api")
 if __name__ == '__main__':
     # uvicorn.run(app, host='https://llm-project-2023.fly.dev', port=8000)
     #uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
-    uvicorn.run(app, host='localhost', port=8001)
+    uvicorn.run(app, host='localhost', port=8000)
