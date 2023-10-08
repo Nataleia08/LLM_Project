998adb48cd6cd3dad6180273c6090777f794aba5
PolinaDyka0 <115357542+PolinaDyka0@users.noreply.github.com>
2023-10-09 00:00:22 +0300

    upload multiple PDF
    added the ability for users to upload multiple PDF documents

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -4,6 +4,7 @@ from cloudinary.uploader import upload
 from app.database.config import config_cloudinary
 from app.database.models import User
 from app.services.auth import auth_service
+from typing import List
 
 
 router = APIRouter(prefix="/upload-pdf", tags=["upload-pdf"])
@@ -17,18 +18,22 @@ config_cloudinary()
 async def display_upload_form(request: Request, current_user: User = Depends(auth_service.get_current_user)):
     return templates.TemplateResponse("index.html", {"request": request, "logged_in": current_user is not None})
 
+
 @router.post("/submit/")
-async def handle_file_upload(file: UploadFile = File(...), current_user: User = Depends(auth_service.get_current_user)):
+async def handle_file_upload(files: List[UploadFile] = File(...), current_user: User = Depends(auth_service.get_current_user)):
 
-    if not file.filename.endswith(".pdf"):
-        raise HTTPException(status_code=400, detail="Invalid file type. Only PDF allowed.")
+    uploaded_files = []
 
-    try:
-        upload_result = None
-        with file.file as f:
-            upload_result = upload(f, resource_type="raw", public_id=f"{file.filename}", folder="files", format="pdf")
-        return {"info": f"file '{file.filename}' uploaded successfully", "url": upload_result['url']}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail="File not uploaded")
+    for file in files:
+        if not file.filename.endswith(".pdf"):
+            raise HTTPException(status_code=400, detail=f"Invalid file type for {file.filename}. Only PDF allowed.")
+        try:
+            with file.file as f:
+                upload_result = upload(f, resource_type="raw", public_id=f"{file.filename}", folder="files", format="pdf")
+                uploaded_files.append({"info": f"file '{file.filename}' uploaded successfully", "url": upload_result['url']})
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"File {file.filename} not uploaded")
+
+    return uploaded_files
 
 
